7df83af063bca82ac07aca451e0eb94e713a1e92
Petroffskiy <petrovskiy.a15@gmail.com>
2023-05-16 09:06:18 +0300

    Добавил блок для корзины

--- lib/core/application.dart
+++ lib/core/application.dart
@@ -6,9 +6,9 @@ import 'package:internet_magazine/core/bottom_bar/bloc/app_bar_bloc.dart';
 import 'package:internet_magazine/core/routers/app_router.dart';
 import 'package:internet_magazine/feature/presentation/auth/authenticate/bloc/authentication_bloc.dart';
 import 'package:internet_magazine/feature/presentation/auth/authorization/bloc/authorization_bloc.dart';
+import 'package:internet_magazine/feature/presentation/basket/bloc/busket/busket_bloc.dart';
 import 'package:internet_magazine/feature/presentation/main_cards/bloc/main_card/main_card_bloc.dart';
 import 'package:internet_magazine/feature/presentation/personal/bloc/personal_bloc/personal_bloc.dart';
-import 'package:internet_magazine/feature/presentation/personal/bloc/visual_bloc/visible_bloc.dart';
 
 import 'injection.dart' as inj;
 
@@ -25,6 +25,7 @@ class Application extends StatelessWidget {
             create: (context) =>
                 inj.inject<AuthenticationBloc>()..add(const CheckHive())),
         BlocProvider(create: (context) => inj.inject<AuthorizationBloc>()),
+        BlocProvider(create: (context) => inj.inject<BusketBloc>()..add(const GetBusketData())),
         BlocProvider(create: (context) => inj.inject<MainCardBloc>()),
         BlocProvider(create: (context) => inj.inject<AppBarBloc>()),
         BlocProvider(create: (context) => inj.inject<PersonalBloc>()),
