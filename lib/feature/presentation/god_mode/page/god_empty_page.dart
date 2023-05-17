4c9adc0823f72948f3310b373c80864861b796b6
Petroffskiy <32158078+Petroffskiy@users.noreply.github.com>
2023-05-17 10:04:48 +0300

    страницы с добавлением бллоков и продукта
    обновил путь до parameter_product

--- lib/feature/presentation/god_mode/page/god_empty_page.dart
+++ lib/feature/presentation/god_mode/page/god_empty_page.dart
@@ -4,6 +4,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:internet_magazine/core/injection.dart' as inj;
 import 'package:internet_magazine/feature/presentation/god_mode/bloc/change_bloc/change_bloc.dart';
+import 'package:internet_magazine/feature/presentation/god_mode/bloc/god_product/god_product_bloc.dart';
+import 'package:internet_magazine/feature/presentation/god_mode/bloc/image_picker/image_picker_bloc.dart';
+import 'package:internet_magazine/feature/presentation/god_mode/bloc/parameter_product_bloc/parameter_product_bloc.dart';
+import 'package:internet_magazine/feature/presentation/god_mode/bloc/select_category/select_category_bloc.dart';
 
 @RoutePage()
 class GodEmptyPage extends StatelessWidget {
@@ -13,7 +17,17 @@ class GodEmptyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (BuildContext context) => inj.inject<ChangeBloc>())
+        BlocProvider(
+            create: (BuildContext context) => inj.inject<ChangeBloc>()),
+        BlocProvider(
+            create: (BuildContext context) => inj.inject<GodProductBloc>()),
+        BlocProvider(
+            create: (BuildContext context) => inj.inject<CategoryBloc>()),
+        BlocProvider(
+            create: (BuildContext context) => inj.inject<ImagePickerBloc>()),
+        BlocProvider(
+            create: (BuildContext context) =>
+                inj.inject<ParameterProductBloc>())
       ],
       child: const AutoRouter(),
     );
--- lib/feature/presentation/god_mode/page/god_product_page.dart
+++ lib/feature/presentation/god_mode/page/god_product_page.dart
@@ -1,10 +1,10 @@
-import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:internet_magazine/assets/colors/colors.dart';
-import 'package:internet_magazine/feature/presentation/god_mode/bloc/change_bloc/change_bloc.dart';
+import 'package:internet_magazine/core/routers/app_router.gr.dart';
 import 'package:internet_magazine/feature/presentation/god_mode/bloc/god_data/god_data_bloc.dart';
-import 'package:internet_magazine/feature/presentation/god_mode/widgets/widget_create.dart';
+import 'package:internet_magazine/feature/presentation/god_mode/bloc/god_product/god_product_bloc.dart';
 import 'package:internet_magazine/feature/presentation/widgets/image_network.dart';
 
 @RoutePage()
@@ -129,7 +129,10 @@ class GodProductPage extends StatelessWidget {
           Align(
             alignment: AlignmentDirectional.bottomEnd,
             child: GestureDetector(
-              onTap: () {},
+              onTap: () {
+                context.read<GodProductBloc>().add(GetGadget());
+                context.router.push(const GodNewProductRoute());
+              },
               child: Container(
                 decoration: BoxDecoration(
                     color: mainColorBlue,
