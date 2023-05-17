c12eaa324d886b10848c65a216148ea2011c45db
Petroffskiy <32158078+Petroffskiy@users.noreply.github.com>
2023-05-17 10:08:17 +0300

    Репозитории для обработки запроса к серверу

--- lib/feature/data/repository/i_god_repository.dart
+++ lib/feature/data/repository/i_god_repository.dart
@@ -1,5 +1,6 @@
 import 'package:internet_magazine/feature/data/api/api_util.dart';
-import 'package:internet_magazine/feature/domain/model/god/primary_god_products_model_domain.dart';
+import 'package:internet_magazine/feature/domain/model/god/product/primary_god_gadgets_model_domain.dart';
+import 'package:internet_magazine/feature/domain/model/god/product/primary_god_products_model_domain.dart';
 import 'package:internet_magazine/feature/domain/repository/i_god_repository.dart';
 
 class IGodRepository extends IGodRepositoryDomain {
@@ -8,5 +9,10 @@ class IGodRepository extends IGodRepositoryDomain {
   @override
   Future<PrimaryGodProductsModelDomain> getGodData() {
     return _apiUtil.getGodData();
+  }
+
+  @override
+  Future<PrimaryGodGadgetsModelDomain> getGadget() {
+    return _apiUtil.getGodGadgets();
   }
 }
--- lib/feature/domain/repository/i_god_repository.dart
+++ lib/feature/domain/repository/i_god_repository.dart
@@ -1,5 +1,7 @@
-import 'package:internet_magazine/feature/domain/model/god/primary_god_products_model_domain.dart';
+import 'package:internet_magazine/feature/domain/model/god/product/primary_god_gadgets_model_domain.dart';
+import 'package:internet_magazine/feature/domain/model/god/product/primary_god_products_model_domain.dart';
 
 abstract class IGodRepositoryDomain {
   Future<PrimaryGodProductsModelDomain> getGodData();
+  Future<PrimaryGodGadgetsModelDomain> getGadget();
 }
